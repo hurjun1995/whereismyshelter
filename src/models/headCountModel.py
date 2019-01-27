60bb034604a4a330437d6b98221eca67e0acd251
hurjun1995 <hurjun1995@gmail.com>
2019-01-26 18:27:19 -0800

    headCountModel added; AccountSchema and ShelterSchema are added, but they aren't done yet

---
+++ src/models/headCountModel.py
@@ -0,0 +1,48 @@
+from marshmallow import fields, Schema
+import datetime
+from . import db
+
+class HeadCountModel(db.Model):
+    """
+    Account Model
+    """
+
+    # table name
+    __tablename__ = 'headcounts'
+
+    id = db.Column(db.Integer, primary_key=True)
+    created_at = db.Column(db.DateTime)
+
+    # class constructor
+    def __init__(self, data):
+        """
+        Class constructor
+        :param data:
+        """
+        self.created_at = datetime.datetime.utcnow()
+        self.modified_at = datetime.datetime.utcnow()
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self, data):
+        for key, item in data.items():
+            setattr(self, key, item)
+        self.modified_at = datetime.datetime
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    @staticmethod
+    def get_all_headCounts():
+        return HeadCountModel.query.all()
+
+    @staticmethod
+    def get_headCount_by_id(id):
+        return HeadCountModel.query.get(id)
+
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
