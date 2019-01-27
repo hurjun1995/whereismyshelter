ec5ce6b0641bdc393fb4a140c26aaa054ddbdd2c
hurjun1995 <hurjun1995@gmail.com>
2019-01-26 21:53:24 -0800

    added manage.py for migration

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 
 from .config import app_config
+from .models import db, bcrypt
 
 
 def create_app(env_name):
@@ -12,6 +13,9 @@ def create_app(env_name):
     app = Flask(__name__)
 
     app.config.from_object(app_config[env_name])
+
+    bcrypt.init_app(app)
+    db.init_app(app)
 
     @app.route('/', methods=['GET'])
     def index():
